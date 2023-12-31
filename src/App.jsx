ade4f944fc10842f8cbe0a0fd9f24cc80e4a461d
sourabhshukla <sourabhshukla9893502147@gmail.com>
2023-12-31 13:48:10 +0530

    Added useContext to manage video files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { createContext, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import VideoPlayer from "./components/VideoPlayer";
+export const VideoContext = createContext();
 function App() {
+  const [currVideo, setCurrVideo] = useState();
+  const [allVideos, setAllVideos] = useState();
+
   return (
-    <div className="flex w-full flex-col">
-      <Header />
-      <VideoPlayer />
-    </div>
+    <VideoContext.Provider
+      value={(currVideo, setCurrVideo, allVideos, setAllVideos)}
+    >
+      <div className="flex w-full flex-col">
+        <Header />
+        <VideoPlayer />
+      </div>
+    </VideoContext.Provider>
   );
 }
 
