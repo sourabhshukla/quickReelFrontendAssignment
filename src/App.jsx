004b88da23d85233f47c5b11ef6265378f2cbac7
sourabhshukla <sourabhshukla9893502147@gmail.com>
2023-12-31 12:31:48 +0530

    Created Header Component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="flex w-full">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="w-full h-16 bg-base-200 flex justify-end items-center pr-4">
+      <button className="btn btn-primary text-lg cursor-pointer">Upload</button>
+    </header>
+  );
+}
